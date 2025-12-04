e82b5d4e6b39984e28d319a08c9849953fce127a
mlatta824 <mlatta824@gmail.com>
2025-12-03 23:10:09 -0500

    Add server pack

--- src/pack_list/page.tsx
+++ src/pack_list/page.tsx
@@ -5,6 +5,8 @@ export default function PackList() {
     const drugpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/Critical.Mass-.Project.MDMA.Mk.II.zip';
     const nukepack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/Scorched.Horizons.zip';
 
+    const serverpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/critical-mass-server.zip';
+
     return (
 
         <div className="pack-list-container">
@@ -15,9 +17,14 @@ export default function PackList() {
             <div className = "card">
                 <h2>Critical Mass: Project MDMA MK II</h2>
                 <p>CURRENT SERVER PACK</p>
-
+                
                 <a href = {drugpack} download className = "download-link">
-                    <button>Download File</button>
+                    <button>Download Modpack</button>
+                </a>
+                <p></p>
+                
+                <a href = {serverpack} download className = "download-link">
+                    <button>Download Server Pack</button>
                 </a>
             </div>
 
@@ -25,7 +32,7 @@ export default function PackList() {
                 <h2>Scorched Horizons</h2>
 
                 <a href = {nukepack} download className = "download-link">
-                    <button>Download File</button>
+                    <button>Download Modpack</button>
                 </a>
             </div>
 
