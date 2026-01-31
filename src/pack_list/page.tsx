680a35418e39ea724876b74662701148b762d036
mlatta824 <mlatta824@gmail.com>
2026-01-31 14:27:47 -0500

    Updated Current Server Pack

--- src/pack_list/page.tsx
+++ src/pack_list/page.tsx
@@ -5,8 +5,9 @@ export default function PackList() {
     const drugpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/Critical.Mass-.Project.MDMA.Mk.II.zip';
     const nukepack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/Scorched.Horizons.zip';
     const cobblemonpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/CobblemonColonies.zip';
+    const rpgpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/Cisco.s.Fantasy.Medieval.RPG.Tweaked.zip'
 
-    const serverpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/critical-mass-server.zip';
+    const serverpack = 'https://github.com/mlatta824/modpack_site/releases/download/Alpha/CARPG.Dragonfyre.V2F.Beta.Serv.zip';
 
     return (
 
@@ -18,10 +19,20 @@ export default function PackList() {
             <a href = {'https://discord.gg/cqjUuW6NQr'} download className = "discord-link">
                 <button>Discord Server</button>     
             </a>
+            <div className = "card">
+                <h2>Cisco's Tweaked</h2>
+                <p>CURRENT SERVER PACK</p>
+
+                <a href = {rpgpack} download className = "download-link">
+                    <button>Download Modpack</button>
+                </a>
+                <a href = {serverpack} download className = "download-link">
+                    <button>Download Server Pack</button>
+                </a>
+            </div>
 
             <div className = "card">
                 <h2>Cobblemon Colonies</h2>
-                <p>CURRENT SERVER PACK</p>
 
                 <a href = {cobblemonpack} download className = "download-link">
                     <button>Download Modpack</button>
@@ -36,9 +47,7 @@ export default function PackList() {
                 </a>
                 <p></p>
                 
-                <a href = {serverpack} download className = "download-link">
-                    <button>Download Server Pack</button>
-                </a>
+                
             </div>
 
             <div className = "card">
